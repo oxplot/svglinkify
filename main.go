a0cc33f08f193106ed0664af97afed534dae629a
Mansour Behabadi <mansour@oxplot.com>
2018-12-03 23:39:41 +1100

    Update usage to include new internal link functionality

--- main.go
+++ main.go
@@ -87,6 +87,9 @@ hyperlinks for any object by right clicking the object and selecting "Create
 Link". Enter a URL in Href field and save your SVG. Then use svglinkify to
 convert your SVG file.
 
+If the hyper link is '#some-id', an internal link is created which when
+clicked, will pan and zoom onto the object with id 'some-id'.
+
 Usage: svglinkify input.svg output.pdf [options]
 
 `)
